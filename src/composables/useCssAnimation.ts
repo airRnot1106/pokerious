fec8ebf82bb1892d4356a7ffaadcb3eb09492c53
airRnot1106 <yngw.rintaro@gmail.com>
2022-12-26 01:20:40 +0900

    useCssAnimationを追加

---
+++ src/composables/useCssAnimation.ts
@@ -0,0 +1,59 @@
+import type { Ref } from '@/types/type';
+import type { MaybeElement } from '@vueuse/core';
+
+const defaultUseCssAnimationOptions = {
+  endFunc: () => {},
+  duration: 0,
+};
+
+export default (
+  source: Ref<MaybeElement | undefined>,
+  cssClass: string,
+  options?: Partial<typeof defaultUseCssAnimationOptions>
+) => {
+  const { endFunc, duration } = {
+    ...defaultUseCssAnimationOptions,
+    ...options,
+  };
+
+  const isAnimating = ref(false);
+
+  const isEnded = ref(false);
+
+  const play = () => {
+    const element = unrefElement(source);
+    if (!element) {
+      isEnded.value = true;
+      return;
+    }
+
+    isAnimating.value = true;
+
+    element.classList.add(cssClass);
+
+    const cleanup = useEventListener(element, 'animationend', async () => {
+      await new Promise((resolve) => setTimeout(resolve, duration));
+      isAnimating.value = false;
+      isEnded.value = true;
+      endFunc();
+      cleanup();
+    });
+  };
+
+  const reset = () => {
+    const element = unrefElement(source);
+    if (!element) return;
+
+    element.classList.remove(cssClass);
+
+    isAnimating.value = false;
+    isEnded.value = false;
+  };
+
+  return {
+    isAnimating: readonly(isAnimating),
+    isEnded: readonly(isEnded),
+    play,
+    reset,
+  };
+};
