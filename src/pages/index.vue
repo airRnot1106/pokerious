a160a4f324977957c37217674a4e627e2fe48113
airRnot1106 <yngw.rintaro@gmail.com>
2022-12-26 01:21:33 +0900

    heroを追加

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const heroRef = ref<HTMLElement>();
+
+  const { play } = useCssAnimation(heroRef, 'animate-slide-out-bck-center', {
+    endFunc: () => {
+      navigateTo('/game');
+    },
+    duration: 500,
+  });
+</script>
 
 <template>
-  <div></div>
+  <div ref="heroRef" class="hero">
+    <div class="hero-content text-center">
+      <div class="max-w-lg">
+        <h1 class="text-5xl font-bold">Pokerious</h1>
+        <p class="py-6">
+          Your preflop improvement will lead you to victory.<br />
+          This app will help you learn the best action based on hand ranges.
+        </p>
+        <button class="btn-primary btn" @click="play">Get Started</button>
+      </div>
+    </div>
+  </div>
 </template>
