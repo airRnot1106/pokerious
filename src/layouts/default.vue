f33c250c3a0bdfd688648301fc888453f84de035
airRnot1106 <yngw.rintaro@gmail.com>
2022-12-26 01:21:20 +0900

    ページ遷移を追加

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,7 +3,9 @@
 <template>
   <div>
     <header class="flex h-[10dvh] items-center bg-base-200">
-      <h1 class="mx-5 select-none text-3xl font-bold">Pokerious</h1>
+      <NuxtLink to="/">
+        <h1 class="mx-5 select-none text-3xl font-bold">Pokerious</h1>
+      </NuxtLink>
     </header>
     <slot />
   </div>
