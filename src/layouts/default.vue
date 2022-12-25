b21f56717e7ea421cec9829c379d376bfc7af3d2
airRnot1106 <yngw.rintaro@gmail.com>
2022-12-26 00:48:52 +0900

    背景色を変更

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div>
-    <header class="flex h-[10dvh] items-center">
+    <header class="flex h-[10dvh] items-center bg-base-200">
       <h1 class="mx-5 select-none text-3xl font-bold">Pokerious</h1>
     </header>
     <slot />
