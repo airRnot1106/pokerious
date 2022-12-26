8fa84c876bee37224df943337e697aff9e6c64e7
airRnot1106 <yngw.rintaro@gmail.com>
2022-12-26 23:29:20 +0900

    onAdvanceを追加

--- src/composables/useProgress.ts
+++ src/composables/useProgress.ts
@@ -1,13 +1,23 @@
+import type { UnwrapRef } from 'vue';
+
 export default () => {
   const progress = useState('progress', () => 0);
 
   const advance = () => {
     progress.value += 1;
+    progressEvent.trigger(progress.value);
   };
 
   const reset = () => {
     progress.value = 0;
   };
 
-  return { progress: readonly(progress), advance, reset };
+  const progressEvent = createEventHook<UnwrapRef<typeof progress>>();
+
+  return {
+    progress: readonly(progress),
+    advance,
+    reset,
+    onAdvance: progressEvent.on,
+  };
 };
