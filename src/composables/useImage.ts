d7d24dffb9a1b5a5cb334e993678053dccb5eba4
airRnot1106 <yngw.rintaro@gmail.com>
2022-12-26 23:28:41 +0900

    onLoadedを追加

--- src/composables/useImage.ts
+++ src/composables/useImage.ts
@@ -3,13 +3,16 @@ import type { Ref } from '@/types/type';
 export default (src: Ref<string>) => {
   const url = ref('');
 
+  const imageLoadEvent = createEventHook<void>();
+
   const load = () => {
     url.value = new URL(`../assets/images/${src.value}`, import.meta.url).href;
+    setTimeout(() => imageLoadEvent.trigger(), 1); // 1ms is the minimum delay for the image to load
   };
 
   watch(src, load);
 
   onMounted(load);
 
-  return { url: readonly(url) };
+  return { url: readonly(url), onLoaded: imageLoadEvent.on };
 };
