0b4afe2d7f946e441a4bfdb3deb0f302c0bf8e8f
airRnot1106 <yngw.rintaro@gmail.com>
2022-12-28 02:53:44 +0900

    レイアウトを修正

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div>
-    <header class="flex h-[10dvh] items-center bg-base-200">
+    <header class="flex h-[10dvh] items-center justify-between bg-base-200">
       <NuxtLink to="/">
         <h1 class="mx-5 select-none text-3xl font-bold">Pokerious</h1>
       </NuxtLink>
