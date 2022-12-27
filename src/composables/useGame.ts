527a8ecca239a0e672452ba664f40e86cb25118a
airRnot1106 <yngw.rintaro@gmail.com>
2022-12-28 02:56:32 +0900

    変更に伴う修正

--- src/composables/useGame.ts
+++ src/composables/useGame.ts
@@ -1,3 +1,5 @@
+import { isGtoHandRange } from '@/libs/handRange';
+
 export default () => {
   const initGame = () => {
     const { reset: resetProgress, progress } = useProgress();
@@ -20,12 +22,39 @@ export default () => {
 
   const game = useState('game', initGame);
 
+  const results = useState('results', () => ({
+    correct: 0,
+    incorrect: 0,
+    consecutive: 0,
+  }));
+
+  const judge = (isCount = false) => {
+    const { position, hand, actions } = unref(game);
+    const result = isGtoHandRange(position, hand, actions);
+    if (isCount && result) {
+      if (result.isCorrect) {
+        results.value.correct++;
+        results.value.consecutive++;
+      } else {
+        results.value.incorrect++;
+        results.value.consecutive = 0;
+      }
+    }
+    return result;
+  };
+
+  const resetEvent = createEventHook<void>();
+
   const reset = () => {
     game.value = initGame();
+    resetEvent.trigger();
   };
 
   return {
     game: readonly(game),
+    results: readonly(results),
+    judge,
     reset,
+    onReset: resetEvent.on,
   };
 };
