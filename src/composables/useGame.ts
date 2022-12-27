8f69f2e20db821767c1a67a043ec1f98b8381f2f
airRnot1106 <yngw.rintaro@gmail.com>
2022-12-27 15:44:29 +0900

    useActionの追加に伴う変更

--- src/composables/useGame.ts
+++ src/composables/useGame.ts
@@ -3,15 +3,18 @@ export default () => {
     const { reset: resetProgress, progress } = useProgress();
     const { reset: resetPosition, position } = usePosition();
     const { reset: resetHand, hand } = useHand();
+    const { reset: resetAction, actions } = useAction();
 
     resetProgress();
     resetPosition();
     resetHand();
+    resetAction();
 
     return {
       progress: unref(progress),
       position: unref(position),
       hand: unref(hand),
+      actions: unref(actions),
     };
   };
 
