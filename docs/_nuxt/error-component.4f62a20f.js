398f600987798537c4718661efbb6b92c7375d06
airRnot1106 <yngw.rintaro@gmail.com>
2022-12-28 12:20:15 +0900

    generate

--- docs/_nuxt/error-component.4f62a20f.js
+++
@@ -1,1 +0,0 @@
-import{c as r,o as e,a as o}from"./entry.4b337c23.js";const t={__name:"error",setup(a){return r({redirect:"/"}),(c,n)=>(e(),o("button",null,"Clear errors"))}},_=t;export{_ as default};
